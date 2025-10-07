0f3e2cefdee3c9d1a5a82cef2064194f007f64be
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 17:42:42 -0600

    Update table_status.py

--- backend/app/routers/table_status.py
+++ backend/app/routers/table_status.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-from backend.app import crud, schemas
-from backend.app.database import get_db
+from app import crud, schemas
+from app.database import get_db
 import uuid
 
 router = APIRouter(prefix="/table_status", tags=["TableStatus"])
