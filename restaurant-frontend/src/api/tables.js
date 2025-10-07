74c8513ed89380259cd6b3ce45db2c4433452080
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 12:26:43 -0600

    Update tables.js

--- restaurant-frontend/src/api/tables.js
+++ restaurant-frontend/src/api/tables.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://api-restaurante-h08h.onrender.com/";
 
 // Mesas
 export const getTables = async () => {
