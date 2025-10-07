145faaa889a3a333467f77724b524abcc9a2681c
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 12:23:48 -0600

    Update customers.js

--- restaurant-frontend/src/api/customers.js
+++ restaurant-frontend/src/api/customers.js
@@ -1,7 +1,7 @@
 // customers.js
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000"; // Cambia si tu backend está en otra URL
+const API_URL = "https://api-restaurante-h08h.onrender.com/"; // Cambia si tu backend está en otra URL
 
 // Obtener todos los clientes
 export const getCustomers = async () => {
