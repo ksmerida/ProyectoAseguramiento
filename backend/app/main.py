c00c573a4fcc05f2553946fd7edcf5fc1e1128e5
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 13:15:19 -0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Restaurant API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend
+    allow_origins=["http://localhost:3000", "https://api-restaurante-h08h.onrender.com/"],  # React frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
