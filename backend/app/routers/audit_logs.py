b308cf75cec695b55c8fe5b9e1e238b039cc03cf
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 10:42:36 -0600

    Update audit_logs.py

--- backend/app/routers/audit_logs.py
+++ backend/app/routers/audit_logs.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from app import crud, schemas
-from app.database import get_db
+from backend.app.database import get_db
 import uuid
 
 router = APIRouter(prefix="/audit_logs", tags=["AuditLogs"])
