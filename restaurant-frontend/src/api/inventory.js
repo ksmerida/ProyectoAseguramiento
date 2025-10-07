c2143f0cb54c977b675e37644d682cc59ffbc14a
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 12:23:59 -0600

    Update inventory.js

--- restaurant-frontend/src/api/inventory.js
+++ restaurant-frontend/src/api/inventory.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://api-restaurante-h08h.onrender.com/";
 
 // Obtener todos los inventarios
 export const getInventory = async () => {
