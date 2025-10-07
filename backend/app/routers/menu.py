a16c6e33024bdee111b33e3d2ec0fe71971bd8d3
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 17:40:44 -0600

    Update menu.py

--- backend/app/routers/menu.py
+++ backend/app/routers/menu.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from backend.app import crud, schemas
-from backend.app.database import get_db
+from app import crud, schemas
+from app.database import get_db
 
 router = APIRouter()
 
