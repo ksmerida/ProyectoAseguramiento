1cc6f7ef577e4b0d1fb4de2c8d4525ebdc953bef
Keilly Mérida <kfigueroam7@miumg.edu.gt>
2025-10-07 18:03:17 -0600

    Update menu_items.js

--- restaurant-frontend/src/api/menu_items.js
+++ restaurant-frontend/src/api/menu_items.js
@@ -1,7 +1,7 @@
 // menuItems.js
 import axios from "axios";
 
-const API_URL = "https://api-restaurante-h08h.onrender.com";
+const API_URL = "https://proyectoaseguramiento-production.up.railway.app";
 
 // Obtener todos los menu items
 export const getMenuItems = async () => {
